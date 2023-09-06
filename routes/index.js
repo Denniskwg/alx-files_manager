bee7d36b2df0c1a6d920780c04f975c789526dcf
Denniskwg <dkamau476@gmail.com>
2023-09-06 12:34:08 +0300

    adding all files

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
   api.post('/users', UsersController.postNew);
+  api.get('/connect', AuthController.getConnect);
+  api.get('/disconnect', AuthController.getDisconnect);
+  api.get('/users/me', UserController.getMe);
 };
 
 module.exports = injectRoutes;
